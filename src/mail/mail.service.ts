44d22551f8cd0b3d5962fe355df5176f77ff68cb
Josejimenez20 <josejimenez2003w@gmail.com>
2025-09-25 16:52:58 -0600

    node service

--- src/mail/mail.service.ts
+++ src/mail/mail.service.ts
@@ -8,7 +8,6 @@ export class MailService {
   constructor() {
     this.transporter = nodemailer.createTransport({
       service: process.env.MAIL_HOST,
-      port:4000,
       auth: {
         user: process.env.MAIL_USER,
         pass: process.env.MAIL_PASS,
