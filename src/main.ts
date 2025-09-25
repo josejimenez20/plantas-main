e8905dafdf7881c3e6f57e7ce3ed2596885b2304
Josejimenez20 <jose.jimenez20@gmail.com>
2025-09-24 21:41:17 -0600

    cors

--- src/main.ts
+++ src/main.ts
@@ -6,7 +6,7 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: 'http://localhost:5173',
+    origin: '*',
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
     allowedHeaders: '*',
