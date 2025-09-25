d954e46c92a8afc069afe3384470a886b0a7486e
Josejimenez20 <jose.jimenez20@gmail.com>
2025-09-24 21:35:18 -0600

    modficaciones de metodos para peticiones y headers

--- src/main.ts
+++ src/main.ts
@@ -6,12 +6,13 @@ import cookieParser from 'cookie-parser';
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
   app.enableCors({
-    origin: "http://localhost:5173",
-    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
+    origin: 'http://localhost:5173',
+    methods: 'GET,HEAD,PUT,PATCH,POST,DELETE,OPTIONS',
     credentials: true,
-    allowedHeaders: 'Content-Type, Accept, Authorization, X-Requested-With',
+    allowedHeaders: '*',
     exposedHeaders: ['set-cookie'],
   });
+
   app.useGlobalPipes(new ValidationPipe());
   app.use(cookieParser());
   await app.listen(process.env.PORT ?? 3000);
